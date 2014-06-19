aaaef9bc1cf6dd649ef8eeb7861437e6efea137b
ehime <dodomeki@gmail.com>
2014-06-18 17:55:07 -0700

    Extended assignments have been migrated to nested loop

--- runtime.py
+++ runtime.py
@@ -12,13 +12,13 @@ print("%12s%16s" % ("ProblemSize", "Seconds"))
 for count in range(5):
 
     start = time.time()
-
-    # Algorithm start
     work = 1
-    for x in range(problemSize):
-        work += 1
-        work -= 1
-    # Algorithm end
+
+    for j in range(problemSize):
+        for k in range(problemSize):
+            work += 1
+            work -= 1
+
     elapsed = time.time() - start
 
     print("%12d%16.3f" % (problemSize, elapsed))
