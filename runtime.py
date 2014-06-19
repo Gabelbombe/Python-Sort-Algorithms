87ed502bd2cbe61163bbfb58ec20b672f475ca2d
ehime <dodomeki@gmail.com>
2014-06-18 17:52:18 -0700

    Measuring the Run Time of an Algorithm

---
+++ runtime.py
@@ -0,0 +1,25 @@
+#!/usr/local/bin/python
+
+"""
+File: runtime.py 
+Prints the running time for problem sizes that double using a single loop
+"""
+
+import time
+
+problemSize = 10000000
+print("%12s%16s" % ("ProblemSize", "Seconds"))
+for count in range(5):
+
+    start = time.time()
+
+    # Algorithm start
+    work = 1
+    for x in range(problemSize):
+        work += 1
+        work -= 1
+    # Algorithm end
+    elapsed = time.time() - start
+
+    print("%12d%16.3f" % (problemSize, elapsed))
+    problemSize *= 2
